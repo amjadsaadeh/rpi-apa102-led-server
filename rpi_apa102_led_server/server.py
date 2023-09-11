0a6bc0d782826bb4dca5f62bfcc45edf37b9120d
Amjad Saadeh <mail@amjadsaadeh.de>
2023-09-11 09:15:56 +0200

    add set led color stub

--- rpi_apa102_led_server/server.py
+++ rpi_apa102_led_server/server.py
@@ -14,9 +14,20 @@ async def root():
 
 @app.get("/led/{led_id}")
 async def get_led_color(led_id: int):
+    check_led_range(led_id)
+    
+    led_color = dev.get_pixel(led_id)
+    return led_color
+
+@app.post("/led/{led_id}")
+async def set_led_color(led_id: int):
+    check_led_range(led_id)
+    # TODO SET COLOR
+    led_color = dev.get_pixel(led_id)
+    return led_color
+    
+# Helpers
+def check_led_range(led_id: int) -> None:
     if led_id < 0 or led_id > NUM_LEDS:
         raise HTTPException(status_code=404, detail=f'LED with id {led_id} not found. It needs to be in the range betweeb 0 and {NUM_LEDS}.')
-
-    led_color = dev.get_pixel(led_id)
-    return 
-    
+   
